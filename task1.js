dd9ffa61718bc009799012d9f22939d3a7cfe3de
a1789342-jessduong <jessicaduong0812@gmail.com>
2021-03-25 14:40:34 +0000

    task1

--- task1.js
+++ task1.js
@@ -2,7 +2,7 @@
 
 function currentDateTime() {
   var x = document.getElementsByTagName("p"); //get the paragraph line
-  document.getElementById("current_time").innerText = x[1].innerText; //<!-- second p = 1 -->
+  document.getElementById("current_time").innerHTML = x[1].innerHTML; //<!-- second p = 1 -->
 
 
   var d = new Date();
