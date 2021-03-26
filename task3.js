33c4466ca5334e35e7732b3170a71be484978f63
a1789342-jessduong <jessicaduong0812@gmail.com>
2021-03-26 11:20:04 +0000

    task3.2

--- task3.js
+++ task3.js
@@ -5,3 +5,23 @@ var num = 1;
 function mouseOver() {
 	document.getElementById("mcount").innerHTML = num++;
   }
+
+//<!-- Task 3.2-->
+
+var display = "";
+
+function mouseClick() {
+
+    var date = new Date();
+
+    var time = document.getElementsByClassName("post-time")
+
+    var post = document.getElementsByClassName("post-content");
+
+    display = display+date+"<br/>"+"<br/>"+post[0].value+"<br/>"+"<br/>";
+
+    time[0].innerHTML = display;
+
+
+
+}
